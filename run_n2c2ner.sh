f090768d87ddeaed36983a335f6cf4cc8dc5dafd
Jiacheng Zhang <zjccjz@hotmail.com>
2021-11-11 11:06:00 -0700

    whatever i added...

--- run_n2c2ner.sh
+++ run_n2c2ner.sh
@@ -19,7 +19,7 @@ task_name=$2
 model_name=$3
 
 MODEL_NAME='ngram=-1.cython_generator=True.pretrain_data=mimic_iii'
-MODEL_NAME='electra_small_owt'
+# MODEL_NAME='electra_small_owt'
 MODEL_DIR='/xdisk/bethard/jiachengz/electra_pretraining/pretrain_data/models/'$MODEL_NAME
 DATA_DIR=/xdisk/bethard/jiachengz/n2c2_2010
 hparam="{\"task_names\": [\"$task_name\"]}"
